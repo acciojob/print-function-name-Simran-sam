4dfeecc645def34202b269b698c507624d658f6d
Simran Pathan <97839297+Simran-sam@users.noreply.github.com>
2025-03-13 22:49:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function functionName(){
+	return functionName.name;
+}
+
+function AccioJob(){
+	return AccioJob.name;
+}
+alert(functionName());
+alert(AccioJob());
